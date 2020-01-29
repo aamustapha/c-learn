4d34c652f36220e37f4f7204c650ed77ee039316
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2020-01-29 15:59:22 +0100

    Add logic

--- input.c
+++ input.c
@@ -2,8 +2,11 @@
 
 int main() {
     char str[50];
+    int age;
     printf("What is your name: ");
     scanf("%s", str);
-    printf("Hello %s, how are you doing today?", str);
+    printf("How old are you? ");
+    scanf("%d", &age);
+    printf("Hey %s You were born in the year %d", str, 2020 - age);
     return 0;
 }
